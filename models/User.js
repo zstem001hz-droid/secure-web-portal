83db68fb16924833db0747d8af68391ee66e95a0
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-22 15:51:10 -0400

    feat: add bcrypt pre-save hook supporting local and OAuth users

--- models/User.js
+++ models/User.js
@@ -28,3 +28,11 @@ const userSchema = new Schema({
     type: String,
   },
 });
+
+// Pre-save hook to hash password — only fires if password is present
+userSchema.pre("save", async function () {
+  if (this.password && (this.isNew || this.isModified("password"))) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+});
