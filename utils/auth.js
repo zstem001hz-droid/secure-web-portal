c6b2921b446d1f44faa2b2499c44a396781d3264
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-22 16:26:42 -0400

    feat: add authMiddleware to utils/auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -11,4 +11,35 @@ module.exports = {
     const payload = { username, email, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
+
+  // Authenticate and decode JWT from request
+  authMiddleware: function (req, res, next) {
+    // Allow token to be sent via body, query, or headers
+    let token =
+      (req.body && req.body.token) ||
+      req.query.token ||
+      req.headers.authorization;
+
+    // Extract token from Bearer scheme
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "You must be logged in to do that." });
+    }
+
+    try {
+      // Verify token and attach decoded user data to request
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+      return res.status(401).json({ message: "Invalid token." });
+    }
+
+    next();
+  },
 };
